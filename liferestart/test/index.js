7538e596086454636d96f36a597b4517de06d2e9
yuhangweb.github.io <yu1309423201@gmail.com>
2021-09-05 15:26:54 +0800

    Update index.js

--- liferestart/test/index.js
+++ liferestart/test/index.js
@@ -9,11 +9,11 @@ async function debug() {
     await life.initial();
 
     life.restart({
-        CHR: 5,                     // 颜值 charm CHR
-        INT: 5,                     // 智力 intelligence INT
-        STR: 5,                     // 体质 strength STR
-        MNY: 5,                     // 家境 money MNY
-        SPR: 5,                     // 快乐 spirit SPR
+        CHR: 20,                     // 颜值 charm CHR
+        INT: 20,                     // 智力 intelligence INT
+        STR: 20,                     // 体质 strength STR
+        MNY: 20,                     // 家境 money MNY
+        SPR: 20,                     // 快乐 spirit SPR
         TLT: [1004, 1005, 1009],    // 天赋 talent TLT
     });
     const lifeTrajectory = [];
